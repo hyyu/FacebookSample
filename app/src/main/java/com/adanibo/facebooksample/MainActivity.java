fbda3ed8c26e801ced7acb3ab7e92b3d9c0b9660
Antoine Danibo <antoine.danibo@epitech.eu>
2016-10-14 12:20:43 +0200

    facebook features

--- app/src/main/java/com/adanibo/facebooksample/MainActivity.java
+++ app/src/main/java/com/adanibo/facebooksample/MainActivity.java
@@ -1,42 +1,94 @@
 package com.adanibo.facebooksample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.share.model.ShareContent;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+import com.facebook.share.widget.ShareDialog.Mode;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity {
+
+    private static final int    FACEBOOK_REQUEST_CODE = 64206;
+
+    private CallbackManager     mCallbackManager;
+    private ShareContent        mShareLinkContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
         AppEventsLogger.activateApp(this);
+
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(getApplicationContext(), loginResult.toString(), Toast.LENGTH_SHORT)
+                                .show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
 
         setContentView(R.layout.activity_main);
 
-        LinearLayout fbBtnScn = (LinearLayout) findViewById(R.id.fb_button);
-        fbBtnScn.setOnClickListener(this);
+        LinearLayout fbLogScn = (LinearLayout) findViewById(R.id.fb_button);
+        fbLogScn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "user_friends"));
+            }
+        });
+
+        LinearLayout fbShareScn = (LinearLayout) findViewById(R.id.fb_share);
+        fbShareScn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mShareLinkContent = new ShareLinkContent.Builder()
+                        .setContentTitle("Test share API Facebook")
+                        .setContentDescription("Test de l'appli FacebookSample")
+                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                        .build();
+                ShareDialog shareDialog = new ShareDialog(MainActivity.this);
+                shareDialog.show(mShareLinkContent, Mode.AUTOMATIC);
+            }
+        });
+
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.activity_main:
-                ArrayList<String> perms = new ArrayList<>();
-                perms.add("publish_actions");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case FACEBOOK_REQUEST_CODE:
+                mCallbackManager.onActivityResult(requestCode, resultCode, data);
 
-                LoginManager.getInstance().logInWithPublishPermissions(this, perms);
             default:
                 break;
         }
